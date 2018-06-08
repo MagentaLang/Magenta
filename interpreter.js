a753bcfa525177b61f42dd1ee2fb8721a31e3137
MininMobile <miningamer@gmail.com>
2018-06-08 08:54:59 +0100

    Add Basic Lexer

--- interpreter.js
+++ interpreter.js
@@ -1,14 +1,32 @@
 var interpreter = {};
 
-interpreter["STATES"] = {
-	regular: 0,
-	string: 1
-};
-
 interpreter["lex"] = (input) => {
 	var tokens = [];
 	var token = "";
-	var state = interpreter.STATES.regular;
+	var state = false; // false = 0, true = 1
+
+	for (let i = 0; i < input.length; i++) {
+		token += input[i];
+		switch (token) {
+			case " ":
+				if (!state) token = "";
+				break;
+
+			case "\n":
+				token = "";
+				break;
+
+			case "Log":
+				tokens.push(token);
+				token = "";
+				break;
+
+			case "\"":
+				state = !state;
+				if (!state) tokens.push(token);
+				break;
+		}
+	}
 
 	return tokens;
 }
