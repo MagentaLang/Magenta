2d5172b82c2d0a0cd14cd9cb8df7c0851be29b0e
MininMobile <miningamer@gmail.com>
2018-06-08 16:18:02 +0100

    Lex Strings

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const fs = require("fs");
 if (process.argv[2] == null) {
 	console.log("No File Specified");
 } else {
-	fs.readFile(process.argv[2], (err, data) => {
+	fs.readFile(process.argv[2], "utf8", (err, data) => {
 		if (err) throw new Error(err);
 
 		let tokens = interpreter.lex(data);
+		console.log(tokens);
 	})
 }
--- interpreter.js
+++ interpreter.js
@@ -3,28 +3,40 @@ var interpreter = {};
 interpreter["lex"] = (input) => {
 	var tokens = [];
 	var token = "";
+	string = "";
 	var state = false; // false = 0, true = 1
 
 	for (let i = 0; i < input.length; i++) {
 		token += input[i];
 		switch (token) {
 			case " ":
-				if (!state) token = "";
+				if (state == false) token = "";
 				break;
 
 			case "\n":
 				token = "";
 				break;
+			
+			case ";":
+				token = "";
+				break;
 
-			case "Log":
+			case "print":
 				tokens.push(token);
 				token = "";
 				break;
 
 			case "\"":
+				console.log(state)
 				state = !state;
-				if (!state) tokens.push(token);
+				console.log(state)
+				if (state == false) tokens.push(string);
 				break;
+		}
+
+		if (state == 1) {
+			if (token != "\"") string += token;
+			token = "";
 		}
 	}
 
